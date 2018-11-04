21e8d7cdc97ffa1082b362e5606ad11f2b24ea92
StepanChornoloz <sprewell911@gmail.com>
2018-11-04 20:38:33 +0200

    published

--- src/homework_13/script/watch.js
+++ src/homework_13/script/watch.js
@@ -11,21 +11,37 @@ import '../styles/watch.scss';
    // }   
 
    export function digitalClock () {
-      
-       const clocks = document.querySelectorAll('clock');
+       
        let date = new Date();
        let hours = date.getHours ();
        let minutes = date.getMinutes ();
        let seconds = date.getSeconds();
+       let day = date.getDate();
+       let month = date.getMonth();
+       let year = date.getFullYear();
 
        if (hours < 10) hours = "0" + hours;
        if (minutes < 10) minutes = "0" + minutes;
        if (seconds < 10) seconds = "0" + seconds;
+       if (day < 10) day = "0" + day;
+       if (month< 10) month = "0" + month;
 
-        document.getElementById("clock").innerHTML = hours+ ':' +minutes+ ':'  + seconds;
+        document.getElementById("clock").innerHTML = hours+ ':' +minutes+ ':' + seconds;
 
         setInterval (digitalClock, 1000);
 
+        document.getElementById("clock").addEventListener('click', function () {
+            document.getElementById("clock").innerHTML = hours+ ':' +minutes;
+           // setInterval (digitalClock, 1000);
+        })
+        
+        document.getElementById("clock").addEventListener('contextmenu', function () {
+            document.getElementById("clock").innerHTML = hours+ ':' +minutes+ ':' + seconds+ '/' +day+ '/' +month+ '/' +year;
+           // setInterval (digitalClock, 1000);
+        })
+
     }
+
+    
     
  
