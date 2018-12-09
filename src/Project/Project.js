c8009de2c48ad580d126bb2e65fa2f3a076c67e4
StepanChornoloz <sprewell911@gmail.com>
2018-12-09 11:20:42 +0200

    added

--- src/Project/Project.js
+++ src/Project/Project.js
@@ -1,1 +1,17 @@
 import './project.scss';
+
+let selectPlanButtons = document.querySelectorAll(".banner__box_container button");
+let burgerButton = document.querySelector('.burger');
+let mobileNav = document.querySelector('.navigation_desc');
+
+
+for (var i = 0; i < selectPlanButtons.length; i++) {
+    selectPlanButtons[i].addEventListener("click", function() {
+        mobileNav.style.display = "block";
+    });
+  }
+
+burgerButton.addEventListener('click', function() {
+    mobileNav.style.display = 'block';
+});
+
