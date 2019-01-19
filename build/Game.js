dd051fa8d0a7b7f4b1901c24a5b7b6eaa87d419e
StepanChornoloz <sprewell911@gmail.com>
2019-01-19 09:43:52 +0200

    added game

--- build/Game.js
+++ build/Game.js
@@ -79,7 +79,11 @@ module.exports = __webpack_require__(357);
 "use strict";
 
 
-__webpack_require__(358);
+/*- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game*/
 
 var scores, roundScore, activePlayer, gamePlaying;
 
@@ -87,21 +91,19 @@ init();
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
     if (gamePlaying) {
-        // 1. Random number
+
         var dice = Math.floor(Math.random() * 6) + 1;
 
-        //2. Display the result
         var diceDOM = document.querySelector('.dice');
         diceDOM.style.display = 'block';
-        diceDOM.src = './image/dice-' + dice + '.png';
+        diceDOM.src = 'image/dice-' + dice + '.png';
 
-        //3. Update the round score IF the rolled number was NOT a 1
         if (dice !== 1) {
-            //Add score
+
             roundScore += dice;
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
         } else {
-            //Next player
+
             nextPlayer();
         }
     }
@@ -109,13 +111,11 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
 
 document.querySelector('.btn-hold').addEventListener('click', function () {
     if (gamePlaying) {
-        // Add CURRENT score to GLOBAL score
+
         scores[activePlayer] += roundScore;
 
-        // Update the UI
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-        // Check if player won the game
         if (scores[activePlayer] >= 100) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
@@ -123,14 +123,14 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
             gamePlaying = false;
         } else {
-            //Next player
+
             nextPlayer();
         }
     }
 });
 
 function nextPlayer() {
-    //Next player
+
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
 
@@ -165,13 +165,6 @@ function init() {
     document.querySelector('.player-1-panel').classList.remove('active');
     document.querySelector('.player-0-panel').classList.add('active');
 }
-
-/***/ }),
-
-/***/ 358:
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
 
 /***/ })
 
